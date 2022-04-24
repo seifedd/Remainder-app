5d42bd9ad716b04e042a69e59139618831e629ed
seifedd <seifelmejri@gmail.com>
2022-04-24 14:47:24 -0500

    improved the styling of list items  by adding a CSS components to board

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,6 @@
 import React from "react";
 import List from "./list";
-
+import "./board.css";
 const Info = [
   {
     img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
