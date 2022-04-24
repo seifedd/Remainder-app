7e1c6f8349d4f4aa48821d1f728a2342e314e852
seifedd <seifelmejri@gmail.com>
2022-04-24 15:40:13 -0500

    Remainder(Feat)Upcoming birthdays

--- src/components/board.js
+++ src/components/board.js
@@ -15,7 +15,7 @@ const Info = [
   {
     img: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     name: "Tara Kelly",
-    birthday: "1991-02-02",
+    birthday: "1989-04-24",
   },
   {
     img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
@@ -34,15 +34,42 @@ const Info = [
   },
 ];
 
-//React Functional components have to be defined with capital letters
-function Board() {
+//Render a React Functional components have to be defined with capital letters
+export default function Board() {
   return (
     <div id="site-main">
       <h1 className="text-dark-tile">Birthday Remainder</h1>
       <div className="Board">
-        <List info={Info}></List>
+        <List info={Today(Info)} upcoming={false}></List>
+      </div>
+      <div className="upcoming">
+        <h2 className="text-dark-tile">Upcoming</h2>
+        <List info={Upcoming(Info)} upcoming={true}></List>
       </div>
     </div>
   );
 }
-export default Board;
+
+function Today(person) {
+  let TodayDay = new Date().getDay();
+  let TodayMonth = new Date().getMonth();
+
+  let filter = person.filter((data) => {
+    let Day = new Date(data.birthday).getDay();
+    let month = new Date(data.birthday).getMonth();
+
+    return TodayDay === Day && TodayMonth === month;
+  });
+  return filter;
+}
+
+function Upcoming(person, toMonth = 3) {
+  let TodayMonth = new Date().getMonth();
+
+  let filter = person.filter((data) => {
+    let Month = new Date(data.birthday).getMonth();
+    return TodayMonth <= Month && Month <= TodayMonth + toMonth;
+  });
+
+  return filter;
+}
--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,11 @@
-export default function list(info) {
-  return <ul>{iterate(info.info)}</ul>;
+export default function list({ info, upcoming }) {
+  console.log(upcoming);
+  return <ul>{iterate(info, upcoming)}</ul>;
 }
 
-function iterate(data) {
+function iterate(data, flag) {
   if (!data) return;
+  const bgColor = flag ? { backgroundColor: "#ffe66d" } : {};
   return data.map((person, index) => {
     const text = person.birthday;
     const regexpLastWord = /[0-9]*/;
@@ -12,7 +14,7 @@ function iterate(data) {
 
     return (
       <li key={index}>
-        <div className="flex">
+        <div className="flex" style={bgColor}>
           <img src={person.img} alt="img" />
           <div className="title">
             <h3 className="name">{person.name}</h3>
